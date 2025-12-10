edbf46c8ed808a12e80a0f9118a2280800194ad0
swapnil3307 <pols5484@gmail.com>
2025-12-10 15:49:34 +0530

    first commit

---
+++ codes/day01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            //use "+"
+            Console.WriteLine("hello :" + "riya");
+
+            //placeholder
+            string name = "riya";
+            string city = "kolhapur";
+            Console.WriteLine("City:{0} Name:{1}", city, name);
+
+            //string imterpolation
+            Console.WriteLine($"City:{city} Name:{name}");
+            #region InputMethod.ReadLine()
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Name:" + name1);
+
+            int num1 = 10;
+            int num2;
+            Console.WriteLine("enter the value:for num2");
+            String value = Console.ReadLine();
+            num2 = int.Parse(value);           //parsing method
+            //num2=convert.ToInt32(value);
+            Console.WriteLine("num2:" + num2);
+            #endregion
+        }
+    }
+}
